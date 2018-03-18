47da5c2750248ed68ac3a0efda76b7a6660eb5b1
Andrew Azlan <azlanappleboy@gmail.com>
2018-03-18 17:51:19 +0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -76,6 +77,6 @@ app.get('/bad', (request, response) => {
 
 //jangan lupa menjalankan perintah ini,
 //callback dapat diberikan pada parameter ke 2
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 });
